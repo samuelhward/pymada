6a48b776f056b9b3c0a9798b2f51528a9d999056
Samuel <samuel.ward@iter.org>
2020-07-10 08:54:39 +0100

    added Base() for describing piece bases for collision detection etc.

---
+++ pymada/classes/base.py
@@ -0,0 +1,12 @@
+import pymada
+import pymada.errors
+
+
+class Base:
+    """Class describing Piece Base
+    """
+
+    def __init__(self):
+        """Constructor for Base
+        """
+        pass
