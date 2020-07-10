8e6c9d1bbeb636e1d42ddac62b414ae47d1710a0
Samuel <samuel.ward@iter.org>
2020-07-10 09:01:05 +0100

    added Dice() class for representing dice pool

---
+++ pymada/classes/dice.py
@@ -0,0 +1,13 @@
+import pymada
+import pymada.errors
+
+
+class Dice:
+    """Class describing dice pool
+    """
+
+    def __init__(self):
+        """Constructor for dice pool
+        """
+
+        pass
