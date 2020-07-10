ce3316693df9e799d1394c31b3263649e927da30
Samuel <samuel.ward@iter.org>
2020-07-10 09:06:04 +0100

    added Fleet() class for representing whole fleet

---
+++ pymada/classes/fleet.py
@@ -0,0 +1,12 @@
+import pymada
+import pymada.errors
+
+
+class Fleet:
+    """Class describing fleet
+    """
+
+    def __init__(self):
+        """Constructor for fleet
+        """
+        pass
