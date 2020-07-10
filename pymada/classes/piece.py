33d008073be00933fdbfd80143c3132f69de5bb4
Samuel <samuel.ward@iter.org>
2020-07-10 08:53:55 +0100

    added absolute paths to Piece with TypeError message

--- pymada/classes/piece.py
+++ pymada/classes/piece.py
@@ -1,12 +1,14 @@
-from .position import Position
+import pymada
+import pymada.errors
+from pymada.classes.position import Position
 
 
 class Piece:
     """Base class describing board pieces
     """
 
-    def __init__(self, position):
-        self.position = position
+    def __init__(self):
+        pass
 
     @property
     def position(self):
@@ -16,5 +18,5 @@ class Piece:
     def position(self, position):
 
         if not isinstance(position, Position):
-            raise TypeError()
+            raise TypeError("Piece.position must be of type Position")
         self._position = position
