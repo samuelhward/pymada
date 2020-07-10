2c6ab33c05da795fb3b7e691cc72509291f424e9
Samuel <samuel.ward@iter.org>
2020-07-10 09:07:05 +0100

    updated Position class to include theta angle, also now imports errors

--- pymada/classes/position.py
+++ pymada/classes/position.py
@@ -1,6 +1,12 @@
+import pymada
+import pymada.errors
+
+
 class Position:
     """Base class describing positions
     """
 
-    def __init__(self, x=0.0, y=0.0):
-        self.x, self.y = x, y
+    def __init__(self, x=0.0, y=0.0, theta=0.0):
+        """Constructor for position including cartesian coordinates and rotation
+        """
+        self.x, self.y, self.theta = x, y, theta
