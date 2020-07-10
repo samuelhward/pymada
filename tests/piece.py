0104fadc071db391ed7b117ee7af8319019a926f
Samuel <samuel.ward@iter.org>
2020-07-10 09:03:10 +0100

    updated piece tests for position typing

--- tests/piece.py
+++ tests/piece.py
@@ -4,43 +4,33 @@ import os, sys  # explicitly modify path to avoid having to constantly run setup
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pymada
 import pymada.errors
-import pymada.classes.position
-import pymada.classes.piece
+from pymada.classes.position import Position
+from pymada.classes.piece import Piece
+
 
 def create_test_piece():
+    """Creates test piece object instance
     """
-    """
-    position = pymada.classes.position.Position(x=5.0, y=5.0)
-    test_piece = pymada.classes.piece.Piece(position=position)
+
+    test_piece = Piece()
     return test_piece
 
 
 def test_create_test_piece():
+    """Check test piece is of type Piece
     """
+
+    assert isinstance(create_test_piece(), Piece)
+
+
+def test_set_position():
+    """Check set position assigns a position instance
     """
 
-    assert isinstance(
-        create_test_piece().position, type(pymada.classes.position.Position())
-    )
+    test_piece = create_test_piece()
+    test_piece.position = Position()
 
+    assert isinstance(test_piece.position, Position)
 
-"""
-def test_raise_exception_type_error_position():
-
-    with pytest.raises(pymada.errors.PieceException):
-        position = pymada.classes.position(x="4", y=5.0)
-
-def capital_case(x):
-    if not isinstance(x, str):
-        raise TypeError("Please provide a string argument")
-    return x.capitalize()
-
-
-def test_capital_case():
-    assert capital_case("semaphore") == "Semaphore"
-
-
-def test_raises_exception_on_non_string_arguments():
     with pytest.raises(TypeError):
-        capital_case(9)
-"""
+        test_piece.position = 5.0
