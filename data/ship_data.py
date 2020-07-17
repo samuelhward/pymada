8a8d5a9302c6356883ea134c5d6a899e96c5bf09
Samuel <samuel.ward@iter.org>
2020-07-18 00:10:21 +0100

    added test ship data with helper function

---
+++ data/ship_data.py
@@ -0,0 +1,47 @@
+"""ship_data
+"""
+from pymada.classes.die import Die
+
+ships = {}
+
+
+def create_ship_data_template():
+    """ Helper function to initialise ship information
+    """
+
+    ship_template = {}
+    ship_template["hull_zones"] = {}
+    ship_template["shields"] = {}
+    ship_template["armament"] = {}
+    ship_template["move"] = {}
+    ship_template["upgrades"] = {}
+
+    ship_template["command"] = None
+    ship_template["hull"] = None
+    ship_template["points"] = None
+    ship_template["size"] = None  # small, medium, large, giant, flotilla
+    ship_template["engineering"] = None
+    ship_template["squadron"] = None
+
+    return ship_template
+
+
+# define a test ship
+
+ships["test_ship"] = create_ship_data_template()
+ships["test_ship"]["hull_zones"] = ["front", "left", "right", "rear"]
+ships["test_ship"]["shields"]["front"] = 1
+ships["test_ship"]["shields"]["left"] = 1
+ships["test_ship"]["shields"]["right"] = 1
+ships["test_ship"]["shields"]["rear"] = 1
+ships["test_ship"]["armament"]["front"] = [Die('red')]
+ships["test_ship"]["armament"]["left"] = [Die('blue')]
+ships["test_ship"]["armament"]["right"] = [Die('blue')]
+ships["test_ship"]["armament"]["rear"] = [Die('blue')]
+ships["test_ship"]["armament"]["anti-squad"] = []
+ships["test_ship"]["move"][1] = [1]
+ships["test_ship"]["move"][2] = [1, 0]
+ships["test_ship"]["upgrades"]["turbolasers"] = []
+ships["test_ship"]["upgrades"]["size"] = "small"
+ships["test_ship"]["engineering"] = 2
+ships["test_ship"]["squadron"] = 1
