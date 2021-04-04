a0c5c1358c2427460818f640672e145c61a09e9a
armoured_moose <samuelhward@gmail.com>
2021-04-04 01:10:56 +0100

    Setup.py now installs package folder correctly
    - previously installing a level too deep in pymada/

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ import setuptools
 setuptools.setup(
     name="pymada",
     version="0.0.1",
-    packages=setuptools.find_packages(where="pymada"),
-    package_dir={"": "pymada",},
+    packages=['pymada'],
     url="https://github.com/armoured-moose/pymada",
     author="Samuel Ward",
     author_email="samuelhward@gmail.com",
