77f82c2f5a5a7071c7e1b8b01ec6034af7cd3064
armoured_moose <samuelhward@gmail.com>
2021-04-17 00:27:26 +0100

    Removed launch/run_tests shell scripts

--- launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-
-#install step here or line below
-export PYTHONPATH=pymada:$PYTHONPATH
-./run_tests.sh
-python -m pymada.launch
-
--- run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-
-#install step here or line below
-export PYTHONPATH=pymada:$PYTHONPATH
-pytest -s tests/basic.py
